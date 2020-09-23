809bc8dee65315d028084bb08ff250054522d95a
Konstantin Anisimov <rrevenantt@gmail.com>
2020-09-23 17:21:12 +0300

    update CI

--- .travis/before-install-linux-rust.sh
+++ .travis/before-install-linux-rust.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly-2020-01-19 -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly-2020-09-19 -y
 export PATH=$HOME/.cargo/bin:$PATH
 ( rustc --version ; cargo --version ) || true
